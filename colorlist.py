e19379a6c658542cbaea8feb14c6ecba57fbc71c
Anjanakp2020 <76677166+Anjanakp2020@users.noreply.github.com>
2021-02-27 18:05:23 +0530

    Update colorlist.py

--- colorlist.py
+++ colorlist.py
@@ -1,4 +1,5 @@
 color=input("Enter the name of colours(space between colours is needed):")
 color_list=color.split(" ")
-print("The colour list is :",color_list)
-print("The first colour of the list is:",color_list[0],"and the last colour in the list is :",color_list[-1])
+print(color_list)
+print("First colour in the list is",color_list[0])
+print("Last colour in the list is :",color_list[-1])
